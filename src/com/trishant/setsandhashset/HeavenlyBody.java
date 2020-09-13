2be521568af0e295a5ca0d82172fae83924c4c9d
trish1098 <trishant.830@outlook.com>
2020-09-13 15:47:04 +0530

    Updated HeavenlyBody Class

---
+++ src/com/trishant/setsandhashset/HeavenlyBody.java
@@ -0,0 +1,28 @@
+package com.trishant.setsandhashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod =orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getOrbitalPeriod() {
+        return this.orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+}
